d3aed315dcc08c3f5a5ba09645e39aa37694d40c
atabak pouya <pouya@chapman.edu>
2017-11-08 04:05:03 +0000

    adding matrix.c

--- src/matrix/matrix.c
+++ src/matrix/matrix.c
@@ -35,21 +35,21 @@ void mset(MATRIX *m, const MINDEX row, const MINDEX col, const MVALUE v) {
     fprintf(stderr, "ERROR: Indexing matrix outside bounds\n");
     return;
   }
-  // Find the pointer to the block of memory containing 
+  // Find the pointer to the block of memory containing
   // the matrix mat, then jump forward row number of rows
   // and col number of columns.  Inside that block of memory
   // insert the value v.
   //
   // Be sure to understand the following line of code well
   *(m->mat + (m->cols * row) + col) = v;
-  // 
+  //
   // Question: is this the same as the following?
   //   (m->mat)[row][col] = v;
-  // 
+  //
   //
   // This is called "row-major ordering" for matrices, since
   // the memory is arranged with each row being contiguous,
-  // and columns spread out in memory (write out the memory 
+  // and columns spread out in memory (write out the memory
   // on the board if you are confused about what this means)
   //
   // Languages that use row-major ordering:
@@ -58,16 +58,15 @@ void mset(MATRIX *m, const MINDEX row, const MINDEX col, const MVALUE v) {
   // The alternative is "column-major ordering", where the
   // columns are stored contiguously in memory instead
   // (write this out on the board for comparison)
-  // 
+  //
   // Languages that use column-major ordering:
   //   FORTRAN, MATLAB, R, Julia
   //
 }
 
 MVALUE mget(const MATRIX *m, const MINDEX row, const MINDEX col) {
-  /*
-   * CODE GOES HERE
-   */
+
+  return *(m->mat + (m->cols * row) + col);
 }
 
 // Abstraction layer in case implementation of VALUE changes later
@@ -76,16 +75,34 @@ void print_value(const MVALUE v) {
 }
 
 void print_matrix(const MATRIX *m) {
+    int r,c;
   MINDEX maxr, maxc;
   maxr = m->rows;
   maxc = m->cols;
-
+    
   // print values of matrix separated by tabs
   // with each row on a separate line
   printf("Matrix (rows: %d, cols: %d) \n", maxr, maxc);
-  /* 
-   * CODE GOES HERE
-   */
+  for (r = 0 ; r< maxr; r++){
+    for (c = 0; c<maxc; c++ ){
+      printf("%Lf\t",mget(m,r,c));
+    }
+    printf("\n");
+  }
 }
 
 // Implementation for add_matrix goes below
+MATRIX add_matrix(const MATRIX *m1, const MATRIX *m2){
+    MATRIX result = new_matrix(m1->rows,m1->cols);
+
+    int r,c;
+
+    for (r = 0 ; r< m1->rows; r++){
+      for (c =0;  c< m1->cols; c++){
+        mset(&result, r, c, mget(m1,r,c) + mget(m2,r,c));
+      }
+    }
+
+    return result;
+
+}
